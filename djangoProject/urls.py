a14c787c4f41bf4dbc88aa6060e094d847bffec7
kayancali <kayancmuhammet2002@gmail.com>
2022-12-23 23:49:29 +0300

    users giriş

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     path('home/', include('home.urls')),
     path("admin/", admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('login/', views.login_view, name='login_view')
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.contrib.auth import logout, login, authenticate
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 from home.models import Setting
@@ -29,3 +30,20 @@ def profil(request):
     setting = Setting.objects.get(pk=1)
     context = {'setting': setting, 'page':'profil'}
     return render(request, 'profil.html', context)
+
+
+def login_view(request):
+    if request.method=='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+
+            return HttpResponseRedirect('/')
+        else:
+            return HttpResponseRedirect('/login')
+
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting, 'page': 'profil'}
+    return render(request, 'login.html', context)
